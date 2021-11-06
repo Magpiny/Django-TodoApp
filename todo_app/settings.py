8ee578bd4cefacfab932a21f73064bf66147fceb
Magpiny <magpinyb@gmail.com>
2021-11-07 00:08:18 +0300

    deployment settings

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -1,4 +1,4 @@
-import django_heroku
+#import django_heroku
 """
 Django settings for todo_app project.
 
@@ -120,5 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
